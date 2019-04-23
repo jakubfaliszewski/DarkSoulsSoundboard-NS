0e94c3ff92b2c2021e871282454b0e1cb56d4ba3
Jakub Faliszewski <jfaliszewski@interia.pl>
2019-04-23 10:53:28 +0200

    playing sounds

--- dss/src/app/home/home.component.ts
+++ dss/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { Page } from "tns-core-modules/ui/page";
-
+import { TNSPlayer } from 'nativescript-audio-player';
+ // ~ = app directory
 @Component({
     selector: "Home",
     moduleId: module.id,
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    private _player: TNSPlayer;
+
     carvings = [
         {
             name: 'Hello',
@@ -34,13 +37,39 @@ export class HomeComponent implements OnInit {
             url: 'assets/mp3/vg.mp3'
         }
     ]; constructor(private page: Page) {
+        this._player = new TNSPlayer();
+        this._player.debug = true; // set true to enable TNSPlayer console logs for debugging.
+       
 
     }
     public onItemTap(sound) {
         console.log('~/' + sound);
-        
+        this._player
+            .initFromFile({
+                audioFile: '~/' + sound,
+                loop: false,
+                completeCallback: this._trackComplete.bind(this),
+                errorCallback: this._trackError.bind(this)
+            })
+            .then(() => {
+                this._player.getAudioTrackDuration().then(duration => {
+                    console.log(`song duration:`, duration);
+                    this._player.play();
+
+                });
+            });
 
     }
+
+    private _trackComplete(args: any) {
+        console.log('reference back to player:', args.player);
+    }
+
+    private _trackError(args: any) {
+        console.log('reference back to player:', args.player);
+        console.log('the error:', args.error);
+    }
+
     ngOnInit(): void {
         this.page.actionBarHidden = true;
     }
--- dss/webpack.config.js
+++ dss/webpack.config.js
@@ -253,7 +253,8 @@ module.exports = env => {
             new CopyWebpackPlugin([
                 { from: { glob: "fonts/**" } },
                 { from: { glob: "**/*.jpg" } },
-                { from: { glob: "**/*.png" } },
+                { from: "assets/**" },
+                { from: { glob: "**/*.png" } }
             ], { ignore: [`${relative(appPath, appResourcesFullPath)}/**`] }),
             // Generate a bundle starter script and activate it in package.json
             new nsWebpack.GenerateBundleStarterPlugin(
